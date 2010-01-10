43983209e093fdc3ec4bda4442799480050e3754
Rick E <ricky@lightning.local>
2010-01-10 02:22:25 -0500

    add sum.go

---
+++ sum.go
@@ -0,0 +1,16 @@
+// vim: ft=go
+
+package main
+
+func sum(a []int) int {
+   s := 0
+   for i := 0; i < len(a); i++ {
+      s += a[i]
+   }
+   return s
+}
+
+func main() {
+   s := sum([]int{1,2,3})
+   println(s)
+}
